a517ccd9f5027445c1c36f1a656cc95daf4add27
Daniel P <daniel.palomino@utel.edu.mx>
2021-06-12 12:54:10 -0500

    Función al botón de agregar

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ for (let i = 0; i < myNodelist.length; i++) {
   myNodelist[i].appendChild(span);
   
 }
+
 // Al dar click en el botón cerrar se oculta el elemento
 var close = document.getElementsByClassName("close");
 var i;
@@ -17,4 +18,29 @@ for (let i = 0; i < close.length; i++) {
     var div = this.parentElement;
     div.style.display = "none";  
   }
+}
+
+// Agregar nuevas tareas a la lista
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+
+  document.getElementById("myUL").appendChild(li);
+
+  document.getElementById("myInput").value = ""
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("X");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (let i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+
 }
